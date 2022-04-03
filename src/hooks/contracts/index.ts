08aebbf4e9a986eac9a470a01d880d3db61986eb
NolannBiron <biron.nolann@gmail.com>
2022-04-03 23:10:44 +0200

    prettier

--- src/hooks/contracts/index.ts
+++ src/hooks/contracts/index.ts
@@ -1,1 +1,1 @@
-export {default as useERC20} from './useERC20';
+export { default as useERC20 } from "./useERC20";
--- src/hooks/contracts/useERC20.ts
+++ src/hooks/contracts/useERC20.ts
@@ -1,7 +1,5 @@
 import ERC20_ABI from "../../abis/ERC20";
 
-const useERC20 = () => {
-
-}
+const useERC20 = () => {};
 
 export default useERC20;
